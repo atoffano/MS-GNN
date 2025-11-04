99ec39bc33b34992cc3ec7334f384b2b1bba1052
atoffano <antoine.toffano@gmail.com>
2025-11-04 17:31:12 +0100

    gnn model bn -> norm

--- src/models/gnn_model.py
+++ src/models/gnn_model.py
@@ -111,29 +111,29 @@ class ProteinGNN(torch.nn.Module):
         self.prelu1 = torch.nn.ModuleDict(
             {node_type: PReLU(hidden_channels) for node_type in node_types}
         )
-        self.bn1 = torch.nn.ModuleDict(
-            {node_type: BatchNorm(hidden_channels) for node_type in node_types}
+        self.norm1 = torch.nn.ModuleDict(
+            {node_type: LayerNorm(hidden_channels) for node_type in node_types}
         )
         self.conv1 = HeteroGATConv(
             edge_types=edge_types,
             channels=hidden_channels,
         )
         self.prelu_gnn1 = PReLU()
-        self.bn_gnn1 = BatchNorm(hidden_channels)
+        self.norm_gnn1 = LayerNorm(hidden_channels)
 
         self.conv2 = HeteroGATConv(
             edge_types=edge_types,
             channels=hidden_channels,
         )
         self.prelu_gnn2 = PReLU()
-        self.bn_gnn2 = BatchNorm(hidden_channels)
+        self.norm_gnn2 = LayerNorm(hidden_channels)
 
         self.prelu_prot = PReLU()
-        self.bn_prot = BatchNorm(hidden_channels * 2)  # Due to SkipCat
+        self.norm_prot = LayerNorm(hidden_channels * 2)  # Due to SkipCat
 
         self.lin_post = Linear(hidden_channels * 2, hidden_channels)
         self.prelu_post = PReLU()
-        self.bn_post = BatchNorm(hidden_channels)
+        self.norm_post = LayerNorm(hidden_channels)
 
         self.lin_out = Linear(hidden_channels, out_channels)
 
@@ -155,7 +155,7 @@ class ProteinGNN(torch.nn.Module):
         # Input linear
         x_dict = self.lin_in(x_dict)
         x_dict = {k: self.prelu1[k](v) for k, v in x_dict.items()}
-        x_dict = {k: self.bn1[k](v) for k, v in x_dict.items()}
+        x_dict = {k: self.norm1[k](v) for k, v in x_dict.items()}
 
         # GNN layer 1
         x_gnn1, attn1 = self.conv1(
@@ -165,7 +165,7 @@ class ProteinGNN(torch.nn.Module):
             return_attention_weights=return_attention_weights,
         )
         x_gnn1 = {k: self.prelu_gnn1(v) for k, v in x_gnn1.items()}
-        x_gnn1 = {k: self.bn_gnn1(v) for k, v in x_gnn1.items()}
+        x_gnn1 = {k: self.norm_gnn1(v) for k, v in x_gnn1.items()}
         x_gnn1["aa"] = x_dict["aa"]  # Preserve 'aa' node features
 
         # GNN layer 2
@@ -176,7 +176,7 @@ class ProteinGNN(torch.nn.Module):
             return_attention_weights=return_attention_weights,
         )
         x_gnn2 = {k: self.prelu_gnn2(v) for k, v in x_gnn2.items()}
-        x_gnn2 = {k: self.bn_gnn2(v) for k, v in x_gnn2.items()}
+        x_gnn2 = {k: self.norm_gnn2(v) for k, v in x_gnn2.items()}
 
         # SkipCat: concatenate transformed input features and GNN output
         x_prot = torch.cat(
@@ -187,12 +187,12 @@ class ProteinGNN(torch.nn.Module):
             dim=1,
         )
         x_prot = self.prelu_prot(x_prot)
-        x_prot = self.bn_prot(x_prot)
+        x_prot = self.norm_prot(x_prot)
 
         # Post-process lin layers
         x_prot = self.lin_post(x_prot)
         x_prot = self.prelu_post(x_prot)
-        x_prot = self.bn_post(x_prot)
+        x_prot = self.norm_post(x_prot)
 
         x_prot = self.lin_out(x_prot)
 
@@ -204,83 +204,3 @@ class ProteinGNN(torch.nn.Module):
             return x_prot, attentions
 
         return x_prot
-
-
-# class ProteinGNN(torch.nn.Module):
-#     def __init__(self, config, dataset):
-#         super().__init__()
-#         self.edge_attrs = config["model"]["edge_attrs"]
-#         hidden_channels = config["model"]["hidden_channels"]
-#         out_channels = dataset.go_vocab_size
-#         node_types = ["protein", "aa"]
-#         self.lin_in = HeteroDictLinear(
-#             in_channels=-1,
-#             out_channels=hidden_channels,
-#             types=node_types,
-#         )
-#         self.prelu1 = torch.nn.ModuleDict(
-#             {node_type: PReLU(hidden_channels) for node_type in node_types}
-#         )
-#         self.bn1 = torch.nn.ModuleDict(
-#             {node_type: BatchNorm(hidden_channels) for node_type in node_types}
-#         )
-#         self.conv1 = HeteroConv(
-#             {
-#                 ("aa", "belongs_to", "protein"): GATv2Conv(
-#                     in_channels=(-1, -1),
-#                     out_channels=hidden_channels,
-#                     add_self_loops=False,
-#                 ),
-#                 ("protein", "aligned_with", "protein"): GATv2Conv(
-#                     (-1, -1),
-#                     hidden_channels,
-#                     add_self_loops=False,
-#                     edge_dim=(
-#                         dataset.data.num_edge_features[
-#                             ("protein", "aligned_with", "protein")
-#                         ]
-#                         if self.edge_attrs
-#                         else None
-#                     ),
-#                 ),
-#             },
-#             aggr="sum",
-#         )
-#         self.prelu_prot = PReLU()
-#         self.bn_prot = BatchNorm(hidden_channels * 2)  # Due to SkipCat
-
-#         self.lin_post = Linear(hidden_channels * 2, hidden_channels)
-#         self.prelu_post = PReLU()
-#         self.bn_post = BatchNorm(hidden_channels)
-
-#         self.lin_out = Linear(hidden_channels, out_channels)
-
-#     def forward(self, x_dict, edge_index_dict, batch):
-#         edge_attrs_dict = batch.edge_attr_dict if self.edge_attrs else None
-#         x_dict = self.lin_in(x_dict)
-#         x_dict = {k: self.prelu1[k](v) for k, v in x_dict.items()}
-#         x_dict = {k: self.bn1[k](v) for k, v in x_dict.items()}
-#         if self.edge_attrs:
-#             x_gnn = self.conv1(x_dict, edge_index_dict, edge_attr_dict=edge_attrs_dict)
-#         else:
-#             x_gnn = self.conv1(x_dict, edge_index_dict)
-#         # x_dict = self.conv2(x_dict, edge_index_dict, edge_attrs_dict)
-
-#         # SkipCat: concatenate input features and GNN output
-#         x_prot = torch.cat(
-#             [
-#                 x_dict["protein"][: batch["protein"].batch_size],
-#                 x_gnn["protein"][: batch["protein"].batch_size],
-#             ],
-#             dim=1,
-#         )
-#         x_prot = self.prelu_prot(x_prot)
-#         x_prot = self.bn_prot(x_prot)
-
-#         x_prot = self.lin_post(x_prot)
-#         x_prot = self.prelu_post(x_prot)
-#         x_prot = self.bn_post(x_prot)
-
-#         x_prot = self.lin_out(x_prot)
-
-#         return x_prot
