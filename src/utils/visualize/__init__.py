7c29a2ce54ae31bb38a5e89d69302e0bfe998d2e
atoffano <antoine.toffano@gmail.com>
2026-07-30 13:34:09 +0200

    init for rank viz

--- src/utils/visualize/__init__.py
+++ src/utils/visualize/__init__.py
@@ -20,27 +20,35 @@ from src.utils.visualize.rendering import (
     render_scene,
     export_layer_attention_3d,
     export_captum_3d,
+    export_captum_3d_rank,
 )
 
 # -- Network-level plots ----------------------------------------------------
 from src.utils.visualize.network import (
     plot_systemic_attention,
+    plot_systemic_attention_rank,
     plot_systemic_explanation,
+    plot_systemic_explanation_rank,
 )
 
 # -- Residue-level plots ----------------------------------------------------
 from src.utils.visualize.residue import (
     build_protein_score_map,
     plot_protein_attention,
+    plot_protein_attention_rank,
     plot_protein_explanation,
+    plot_protein_explanation_rank,
 )
 
 # -- Comparison / correlation plots -----------------------------------------
 from src.utils.visualize.comparison import (
     analyze_attention_captum_correlation,
     plot_attn_stringdb_vs_aligned_scatter,
+    plot_attn_stringdb_vs_aligned_scatter_rank,
     plot_edge_attr_vs_attention_scatter,
+    plot_edge_attr_vs_attention_scatter_rank,
     plot_shared_name_attention_boxplot,
+    plot_shared_name_attention_boxplot_rank,
 )
 
 __all__ = [
@@ -54,16 +62,24 @@ __all__ = [
     "render_scene",
     "export_layer_attention_3d",
     "export_captum_3d",
+    "export_captum_3d_rank",
     # network
     "plot_systemic_attention",
+    "plot_systemic_attention_rank",
     "plot_systemic_explanation",
+    "plot_systemic_explanation_rank",
     # residue
     "build_protein_score_map",
     "plot_protein_attention",
+    "plot_protein_attention_rank",
     "plot_protein_explanation",
+    "plot_protein_explanation_rank",
     # comparison
     "analyze_attention_captum_correlation",
     "plot_attn_stringdb_vs_aligned_scatter",
+    "plot_attn_stringdb_vs_aligned_scatter_rank",
     "plot_edge_attr_vs_attention_scatter",
+    "plot_edge_attr_vs_attention_scatter_rank",
     "plot_shared_name_attention_boxplot",
+    "plot_shared_name_attention_boxplot_rank",
 ]
